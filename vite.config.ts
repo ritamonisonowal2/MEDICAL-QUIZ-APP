759543a8305e63754c24ab8fa5ad689af5a5c577
Disha Sonowal <ritamonisonowal2@gmail.com>
2026-01-28 17:25:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
